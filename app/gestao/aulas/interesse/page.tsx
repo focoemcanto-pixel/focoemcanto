df769d69d104b2886c3d4d2e240e8d341c657824
focoemcanto-pixel <focoemcanto@gmail.com>
2026-08-15 13:38:23 -0300

    Ativa InterestManagerV2

--- app/gestao/aulas/interesse/page.tsx
+++ app/gestao/aulas/interesse/page.tsx
@@ -1,5 +1,5 @@
-import InterestManager from './InterestManager'
+import InterestManagerV2 from './InterestManagerV2'
 
-export default function InteressePage(){
-  return <InterestManager />
+export default function InterestPage() {
+  return <InterestManagerV2 />
 }
