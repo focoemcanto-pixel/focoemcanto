e12ecf7e8bcd7da098bec912af07bbaa58db1dd9
focoemcanto-pixel <focoemcanto@gmail.com>
2026-08-15 17:18:52 -0300

    feat: expose bulk lead management

--- app/gestao/aulas/interesse/page.tsx
+++ app/gestao/aulas/interesse/page.tsx
@@ -1,5 +1,6 @@
 import InterestManagerV4 from './InterestManagerV4'
+import BulkLeadTools from './BulkLeadTools'
 
 export default function InterestPage() {
-  return <InterestManagerV4 />
+  return <><InterestManagerV4 /><BulkLeadTools /></>
 }
