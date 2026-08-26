2eb4839bca2ff3abc2cf9fe5333c8a612cc4fbe8
focoemcanto-pixel <focoemcanto@gmail.com>
2026-08-26 08:13:29 -0300

    feat: make safe add flow the default

--- app/closet/layout.tsx
+++ app/closet/layout.tsx
@@ -1,9 +1,11 @@
 import type { ReactNode } from 'react';
 import ClosetShellNav from './ClosetShellNav';
+import ClosetAddRedirect from './ClosetAddRedirect';
 
 export default function ClosetLayout({children}:{children:ReactNode}){
  return <>
   {children}
+  <ClosetAddRedirect/>
   <ClosetShellNav/>
  </>;
 }
